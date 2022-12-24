85ba8a23fb0e8871fdcea607ceeca2391131b145
EvillaiN <egorbezzubenko@mail.ru>
2022-12-25 00:06:18 +0300

    leetcode tasks on arrays

---
+++ src/main/java/arrays/MoveZeroes.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+        for (int i = 0, j = 0; j < nums.length; i++, j++) {
+            if (nums[i] == 0) {
+                while (nums[j] == 0 && j < nums.length - 1) {
+                    j++;
+                }
+                nums[i] = nums[j];
+                nums[j] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,0,0,0,0,0,0,0,0};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
